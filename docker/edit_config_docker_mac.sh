17ff84ada7d8993e802bd0dc46a84638dce9526a
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-06-27 00:12:24 -0300

    Shellscripts de Dokcer

---
+++ docker/edit_config_docker_mac.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script para remover a linha "credsStore": "desktop"
+# do aqruivo .dokcer/config.json
+# corrigindo o erro abaixo:
+# error getting credentials - err: exit status 1, out: ``
+
+echo "Type your username: ";
+read user_name;
+
+rm -rf /Users/$user_name/.docker/config.json
+cp config.json /Users/$user_name/.docker/config.json

e08b3ec1a3c6bba71a758cf765352f37f9b52a8f
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-06-29 22:49:46 -0300

    Update get_key_sha1_fingerprint.sh

--- play_store/get_key_sha1_fingerprint.sh
+++ play_store/get_key_sha1_fingerprint.sh
@@ -12,9 +12,14 @@ read storePasss;
 echo "Type your key pass: ";
 read keyPass;
 
-echo "\n\n\n";
-echo "SHA1 generated!";
-echo "\n\n\n";
+echo -e "\n\n\n";
+echo -e "\033[01;32m#######################\033[01;32m"
+echo -e "\033[01;32m### SHA1 generated! ###\033[01;32m"
+echo -e "\033[01;32m#######################\033[01;32m"
+echo -e "\n\n\n";
 
+echo -e "\033[01;32m#######################\033[01;32m"
+echo -e "\n\n";
 keytool -list -v -keystore $directoryKeyStore -alias $alias -storepass $storePasss -keypass $keyPass
-
+echo -e "\n\n";
+echo -e "\033[01;32m#######################\033[01;32m"

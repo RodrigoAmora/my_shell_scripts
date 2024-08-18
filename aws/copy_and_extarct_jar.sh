74f213b68bc92aad70b82599d61539dc36d9db3b
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-08-18 01:02:10 -0300

    Update copy_and_extarct_jar.sh

--- aws/copy_and_extarct_jar.sh
+++ aws/copy_and_extarct_jar.sh
@@ -3,9 +3,16 @@
 echo "Type name of .jar: "
 read nameJar;
 
+if ! [[ $nameJar =~ ".jar" ]]; then
+	echo "jhdvhhgu";
+  	nameJar+=".jar";
+fi
+
 echo "Type the presigned URL: " 
 read presignedURL;
 
 wget -O $nameJar "$presignedURL"
+
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 
 nohup java -jar $nameJar

625d311b9c5bd6d1370535059e28ba117fa9c45a
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-08-19 00:59:09 -0300

    Update copy_and_extarct_jar.sh

--- aws/copy_and_extarct_jar.sh
+++ aws/copy_and_extarct_jar.sh
@@ -10,6 +10,11 @@ fi
 echo "Type the presigned URL: " 
 read presignedURL;
 
+echo -e "\033[01;32m#############################\033[01;32m"
+echo -e "\033[01;32m### Downloading .jar file ###\033[01;32m"
+echo -e "\033[01;32m#############################\033[01;32m"
+echo -e "\n\n\n"
+
 wget -O $nameJar "$presignedURL"
 
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080

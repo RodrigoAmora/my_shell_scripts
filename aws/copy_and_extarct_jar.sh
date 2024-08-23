283123e343b2b19cb0586d2112dd9aedcbadbbe1
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-08-23 00:16:22 -0300

    Update copy_and_extarct_jar.sh

--- aws/copy_and_extarct_jar.sh
+++ aws/copy_and_extarct_jar.sh
@@ -1,22 +1,61 @@
 #!/bin/bash
 
-echo "Type name of .jar: "
-read nameJar;
+USER=$(whoami)
 
-if ! [[ $nameJar =~ ".jar" ]]; then
-  	nameJar+=".jar";
+# the directory to save the JAR file (e.g., in the home directory)
+LOCAL_DIR="/home/${USER}"
+
+# Ensure the local directory exists
+mkdir -p "$LOCAL_DIR"
+
+echo "Choose a option:"
+echo "1) Download jar file from S3 directly (AWS CLI)"
+echo "2) Download jar file from presigned URL"
+echo -e "\n\n"
+echo "Type your choise: "
+read number
+
+jar_file=""
+
+if [ $number -eq 1 ]
+then
+	echo "Type bucket name: "
+	read s3_bucket;
+	
+	echo "Type name of .jar: "
+	read jar_file;
+
+	if ! [[ $jar_file =~ ".jar" ]]; then
+	  	jar_file+=".jar";
+	fi
+
+	echo -e "\n\n\n"
+	echo -e "\033[01;32m#############################\033[01;32m"
+	echo -e "\033[01;32m### Downloading JAR file ###\033[01;32m"
+	echo -e "\033[01;32m#############################\033[01;32m"
+	echo -e "\n\n\n"
+
+	aws s3 cp "s3://$s3_bucket/$jar_file" "$LOCAL_DIR/$jar_file"
+else
+	echo "Type name of .jar: "
+	read jar_file;
+
+	if ! [[ $jar_file =~ ".jar" ]]; then
+	  	jar_file+=".jar";
+	fi
+
+	echo "Type the presigned URL: " 
+	read presignedURL;
+
+	echo -e "\n\n\n"
+	echo -e "\033[01;32m#############################\033[01;32m"
+	echo -e "\033[01;32m### Downloading JAR file ###\033[01;32m"
+	echo -e "\033[01;32m#############################\033[01;32m"
+	echo -e "\n\n\n"
+
+	wget -O $jar_file "$presignedURL"
 fi
 
-echo "Type the presigned URL: " 
-read presignedURL;
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 
-echo -e "\033[01;32m#############################\033[01;32m"
-echo -e "\033[01;32m### Downloading .jar file ###\033[01;32m"
-echo -e "\033[01;32m#############################\033[01;32m"
-echo -e "\n\n\n"
-
-wget -O $nameJar "$presignedURL"
-
-sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8081
-
-nohup java -jar $nameJar
+nohup java -jar $jar_file

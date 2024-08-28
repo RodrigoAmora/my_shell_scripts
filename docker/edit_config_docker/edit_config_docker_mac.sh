d13e157a8d13b7e8921a642d61e6ff59608da58e
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-08-28 01:06:40 -0300

    Update edit_config_docker_mac.sh

--- docker/edit_config_docker/edit_config_docker_mac.sh
+++ docker/edit_config_docker/edit_config_docker_mac.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-# Script para remover a linha "credsStore": "desktop"
-# do aqruivo .dokcer/config.json
-# corrigindo o erro abaixo:
+# Shellscript to remove the line "credsStore": "desktop"
+# from file .dokcer/config.json
+# fixing the error:
 # error getting credentials - err: exit status 1, out: ``
 
-echo "Type your username: ";
-read user_name;
-
-rm -rf /Users/$user_name/.docker/config.json
+USER=$(whoami)
+rm -rf /Users/$USER/.docker/config.json
 
 BASEDIR=$(dirname "$0")
-cp $BASEDIR/config.json /Users/$user_name/.docker/config.json
+cp $BASEDIR/config.json /Users/$USER/.docker/config.json

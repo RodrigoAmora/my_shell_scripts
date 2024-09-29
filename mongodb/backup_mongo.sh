6f37c72971c24a58046850af5cb50cb2ee804425
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-09-29 01:23:46 -0300

    Update backup_mongo.sh

--- mongodb/backup_mongo.sh
+++ mongodb/backup_mongo.sh
@@ -7,13 +7,20 @@
 echo "Type hostname: "
 read hostname
 
+echo -e "\n"
+
 echo "Type port: "
 read port
+
+echo -e "\n"
 
 echo "Type database name:"
 read database_name
 
+echo -e "\n"
+
 echo "Type the output directory: "
 read output_directory
 
+cd $MONGO_HOME/bin
 mongodump --host $hostname --port $port --db $database_name --out $output_directory

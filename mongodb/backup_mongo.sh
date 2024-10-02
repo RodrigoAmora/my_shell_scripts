40c5248bc57604f549e4c057eafb045cfa28f5e2
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-10-02 00:07:28 -0300

    Update backup_mongo.sh

--- mongodb/backup_mongo.sh
+++ mongodb/backup_mongo.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 
-################################################################
-## Shell script to do backup of a database on MongoDB         ##
-## You should Download MongoDB Relational Migrator.           ##
-## Link: https://mongodb.com/try/download/relational-migrator ##
-################################################################
+###################################################################
+## Shell script to resotre of a database on MongoDB.             ##
+## You should Download Mongo Tools.                              ##
+## Link: https://www.mongodb.com/try/download/database-tools     ##
+##                                                               ## 
+## After download Mongo Tools, configure the enviroment variable ##
+## MONGO_TOOLS into your nachine <mongo_toole_directory/bin>     ##
+###################################################################
 
 
 echo "Type hostname: "
@@ -26,5 +29,6 @@ echo -e "\n"
 echo "Type the output directory: "
 read output_directory
 
-cd $MONGO_HOME/bin
+cd $MONGO_TOOLS_HOME/bin
+
 mongodump --host $hostname --port $port --db $database_name --out $output_directory

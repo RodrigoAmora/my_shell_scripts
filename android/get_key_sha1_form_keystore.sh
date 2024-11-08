c45e41778d3fd7e0d1e9ed4058b211f2b3cbd9ff
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-11-08 00:23:17 -0300

    Create get_key_sha1_form_keystore.sh

---
+++ android/get_key_sha1_form_keystore.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Type the path of the your file of .keystore: "
+read directoryKeyStore;
+
+echo "Type your alias: ";
+read alias;
+
+echo "Type your sotre pass: ";
+read storePasss;
+
+echo "Type your key pass: ";
+read keyPass;
+
+echo -e "\n\n\n";
+echo -e "\033[01;32m#######################\033[01;32m"
+echo -e "\033[01;32m### SHA1 generated! ###\033[01;32m"
+echo -e "\033[01;32m#######################\033[01;32m"
+echo -e "\n\n\n";
+
+echo -e "\033[01;32m#######################\033[01;32m"
+echo -e "\n\n";
+
+keytool -list -v -keystore $directoryKeyStore -alias $alias -storepass $storePasss -keypass $keyPass

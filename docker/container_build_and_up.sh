5904cc3478a7a42ada68cc7be4eb1663a759b9ea
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-11-14 00:29:52 -0300

    Update container_build_and_up.sh

--- docker/container_build_and_up.sh
+++ docker/container_build_and_up.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+#############################################
 ### Shellscript to up Docker's containers ###
+#############################################
+
+echo -e "\033[01;32m###########################\033[01;32m"
+echo -e "\033[01;32m### Building images.... ###\033[01;32m"
+echo -e "\033[01;32m###########################\033[01;32m"
+echo -e "\n\n"
 
 sudo docker-compose build
+
+echo -e "\n\n"
+echo -e "\033[01;32m########################\033[01;32m"
+echo -e "\033[01;32m### Uping containers ###\033[01;32m"
+echo -e "\033[01;32m########################\033[01;32m"
+echo -e "\n\n"
+
 sudo docker-compose up -d

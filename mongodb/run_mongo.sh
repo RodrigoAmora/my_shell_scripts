d47bea3a35fd682682357da9b0a6e527f424fd83
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-11-14 00:30:08 -0300

    Update run_mongo.sh

--- mongodb/run_mongo.sh
+++ mongodb/run_mongo.sh
@@ -4,11 +4,12 @@
 ## Shellscript to start MongoDB ##
 ##################################
 
+source ~/.bash_profile
 
 echo -e "\033[01;32m########################\033[01;32m"
 echo -e "\033[01;32m### Starting MongoDB ###\033[01;32m"
 echo -e "\033[01;32m########################\033[01;32m"
-echo -e "\n\n\n"
+echo -e "\n\n"
 
 cd $MONGO_HOME/bin
 sudo ./mongod --dbpath /usr/local/var/mongodb

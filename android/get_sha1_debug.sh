a5aecb7e4b0745fe50250bee3775d057391f7663
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-01-29 00:07:32 -0300

    Update get_sha1_debug.sh

--- android/get_sha1_debug.sh
+++ android/get_sha1_debug.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+###################################
+## Shell script to generate SHA1 ##
+###################################
+
+echo -e "\033[01;32m#######################\033[01;32m"
+echo -e "\033[01;32m### SHA1 generated! ###\033[01;32m"
+echo -e "\033[01;32m#######################\033[01;32m"
+echo -e "\n\n";
+
 keytool -list -v -keystore ~/.android/debug.keystore -alias androiddebugkey -storepass android -keypass android 

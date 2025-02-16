35032358515541be5520de605bbd0ff258e40fec
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-02-16 01:41:11 -0300

    Update build_app_github_actions.sh

--- ios/build_app_github_actions.sh
+++ ios/build_app_github_actions.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+####################################################
+## Shellscript to build iOS app on Github Actions ##
+####################################################
+
 xcodebuild build \
 -scheme Eventos\ TI \
 -destination 'platform=iOS Simulator,name=iPhone 11 Pro'
-

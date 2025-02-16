86219657fcf4fc999e10ce5e6e418b23de19605e
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-02-16 01:41:18 -0300

    Update build_tests_github_actions.sh

--- ios/build_tests_github_actions.sh
+++ ios/build_tests_github_actions.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+###############################################################
+## Shellscript to build test of an iOS app on Github Actions ##
+###############################################################
+
 xcodebuild test \
 -scheme Eventos\ TI \
 -destination 'platform=iOS Simulator,name=iPhone 11 Pro'
-

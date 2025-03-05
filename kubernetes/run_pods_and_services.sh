6da3cc7ea079325abf6a46256d2c212d2f8b8779
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-03-05 00:32:45 -0300

    ConfigMap

--- kubernetes/run_pods_and_services.sh
+++ kubernetes/run_pods_and_services.sh
@@ -4,19 +4,38 @@
 ### Shellscript to run Pods and Service on Kubernetes ###
 #########################################################
 
+echo -e "\033[01;32m#############################\033[01;32m"
+echo -e "\033[01;32m### Running ConfigMap.... ###\033[01;32m"
+echo -e "\033[01;32m#############################\033[01;32m"
+echo -e "\n\n"
+
+kubectl apply -f configmap.yml
+
+kubectl get cm
+
+#####
+
 echo -e "\033[01;32m########################\033[01;32m"
-echo -e "\033[01;32m### Running pods.... ###\033[01;32m"
+echo -e "\033[01;32m### Running Pods.... ###\033[01;32m"
 echo -e "\033[01;32m########################\033[01;32m"
 echo -e "\n\n"
 
 kubectl apply -f pod-simple-app.yml
+kubectl apply -f pod-mongo.yml
+kubectl apply -f pod-redis.yml
+
+#####
 
 echo -e "\033[01;32m############################\033[01;32m"
-echo -e "\033[01;32m### Running services.... ###\033[01;32m"
+echo -e "\033[01;32m### Running Services.... ###\033[01;32m"
 echo -e "\033[01;32m############################\033[01;32m"
 echo -e "\n\n"
 
 kubectl apply -f svc-simple-app.yml
+kubectl apply -f svc-mongo.yml
+kubectl apply -f svc-redis.yml
+
+#####
 
 echo -e "\n"
 

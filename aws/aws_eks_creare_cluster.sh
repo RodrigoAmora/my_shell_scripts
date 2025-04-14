c9bc0801c8b8a5d5434dcc3b0e92c3fb1f19f8ce
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-04-14 02:31:18 -0300

    Create aws_eks_creare_cluster.sh

---
+++ aws/aws_eks_creare_cluster.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+##########################################################
+## Shell script to create a cluster of Kubernate on EKS ##
+##########################################################
+
+aws eks update-kubeconfig --name nome_do_cluster

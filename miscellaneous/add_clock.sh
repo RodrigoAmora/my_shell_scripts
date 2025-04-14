2806858b608a2547c2eedf0b92ad1939060bca12
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-04-14 02:35:32 -0300

    Update add_clock.sh

--- miscellaneous/add_clock.sh
+++ miscellaneous/add_clock.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+##############################################
 ## Shellscript to add a clock into terminal ##
+##############################################
 
 while sleep 1;do tput sc;tput cup 0 $(($(tput cols)-29));date;tput rc;done &
-
-

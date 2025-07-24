a4ced5b1b5e5a048dc174926024bd197e03a9f8b
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-07-24 01:37:58 -0300

    Update aws_cli_install.sh

--- aws/aws_cli_install.sh
+++ aws/aws_cli_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+####################################
+## Shellscript to intsall AWS_CLI ##
+####################################
+
 curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 sudo installer -pkg ./AWSCLIV2.pkg -target /
 aws --version
-

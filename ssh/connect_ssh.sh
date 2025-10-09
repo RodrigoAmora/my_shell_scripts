2394f185d60a9676b102b94f15f48570cd9122f0
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2025-10-09 01:26:54 -0300

    Create connect_ssh.sh

---
+++ ssh/connect_ssh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################################
+## Shell script to connect ssh ##
+#################################
+
+echo "Enter the SSH key path: ";
+read sshKeyPath
+
+echo "Enter the user: ";
+read sshUser
+
+echo "Enter the IP: ";
+read sshIP
+
+ssh -i $sshKeyPath $sshUser@$sshIP
